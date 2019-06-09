01273b2c869245a223b68901cf8472dd5bdf5c44
jescanobre <jescanobre@gmail.com>
2019-06-09 01:51:18 -0300

    ajuste

--- src/main/dahora/connection/ConnectionPostgres.java
+++ src/main/dahora/connection/ConnectionPostgres.java
@@ -5,15 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionPostgres {
-	private final String ip = "localhost";
-	private final Integer port = 5432;
+	
+	//private final String ip = "localhost";
+	//private final Integer port = 5432;
 	private final String user = "postgres";
 	private final String password = "postgres";
-	private final String database = "daHora";
+	//private final String database = "daHora";
 	
 	public Connection getConnection() {
 		try {
-			return DriverManager.getConnection("jdbc:postgresql://"+ip+":"+port+"/"+database, user, password);
+			return DriverManager.getConnection("jdbc:postgresql://localhost:5432/daHora", user, password);
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
--- src/main/dahora/dao/AlunoDAO.java
+++ src/main/dahora/dao/AlunoDAO.java
@@ -130,6 +130,5 @@ public class AlunoDAO {
 		}
 		
 		return false;
-	}
-	
+	}	
 }
